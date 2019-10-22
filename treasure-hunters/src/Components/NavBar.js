15afe69143bbe46a8fff137814686d3d78800341
charles-rogers <c.a.r0g3r5@gmail.com>
2019-10-21 19:31:22 -0500

    got it working

---
+++ treasure-hunters/src/Components/NavBar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Link } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1
+    },
+    appBar: {
+        background: "#000"
+    },
+    button: {
+        fontSize: "18px",
+        display: "flex",
+        justifyContent: "flex-end",
+        marginLeft: "20px"
+    },
+    title: {
+        flexGrow: 1,
+        display: "flex",
+        justifyContent: "flex-start",
+        fontSize: "18px"
+    }
+}));
+
+const NavBar = props => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar className={ classes.appBar} position="static" title="Lambda Treasure Hunt 💰">
+                <Toolbar>
+                    <div className={classes.title}>
+                        <Link
+                            style={{ fontSize: 36, color: "#ffd014", textDecoration: "none" }}
+                            component={RouterLink}
+                            to="/"
+                        >
+                            <span role="img" aria-label="moneybag">
+                                💸💰
+                            </span>{" "}
+                            Treasure Hunters{" "}
+                            <span role="img" aria-label="moneybag">
+                                💰💸
+                            </span>
+                        </Link>
+                    </div>
+                    <Link
+                        component={RouterLink}
+                        to="/home"
+                        style={{ color: "#ffd014", textDecoration: "none" }}
+                        color="secondary"
+                        className={classes.button}
+                    >
+                        Home
+                    </Link>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+
+export default NavBar;
